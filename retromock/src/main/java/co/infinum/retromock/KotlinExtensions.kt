c77e881f576f72f3b4136987b89636aca6ad42ed
BraneRi <branimir.akmadza@infinum.hr>
2020-02-25 16:43:40 +0100

    Unwrapp return type fro continuation and wrap it in retrofit Call

---
+++ retromock/src/main/java/co/infinum/retromock/KotlinExtensions.kt
@@ -0,0 +1,94 @@
+//@file:JvmName("KotlinExtensions")
+//
+//package co.infinum.retromock
+//
+//import kotlinx.coroutines.Dispatchers
+//import kotlinx.coroutines.suspendCancellableCoroutine
+//import retrofit2.*
+//import kotlin.coroutines.resumeWithException
+//
+//inline fun <reified T> Retrofit.create(): T = create(T::class.java)
+//
+//suspend fun <T : Any> Call<T>.await(): T {
+//    return suspendCancellableCoroutine { continuation ->
+//        continuation.invokeOnCancellation {
+//            cancel()
+//        }
+//        enqueue(object : Callback<T> {
+//            override fun onResponse(call: Call<T>, response: Response<T>) {
+//                if (response.isSuccessful) {
+//                    val body = response.body()
+//                    if (body == null) {
+//                        val e = KotlinNullPointerException("Response body type was declared as non-null")
+//                        continuation.resumeWithException(e)
+//                    } else {
+//                        continuation.resume(body)
+//                    }
+//                } else {
+//                    continuation.resumeWithException(HttpException(response))
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<T>, t: Throwable) {
+//                continuation.resumeWithException(t)
+//            }
+//        })
+//    }
+//}
+//
+//@JvmName("awaitNullable")
+//suspend fun <T : Any> Call<T?>.await(): T? {
+//    return suspendCancellableCoroutine { continuation ->
+//        continuation.invokeOnCancellation {
+//            cancel()
+//        }
+//        enqueue(object : Callback<T?> {
+//            override fun onResponse(call: Call<T?>, response: Response<T?>) {
+//                if (response.isSuccessful) {
+//                    continuation.resume(response.body())
+//                } else {
+//                    continuation.resumeWithException(HttpException(response))
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<T?>, t: Throwable) {
+//                continuation.resumeWithException(t)
+//            }
+//        })
+//    }
+//}
+//
+//suspend fun <T : Any> Call<T>.awaitResponse(): Response<T> {
+//    return suspendCancellableCoroutine { continuation ->
+//        continuation.invokeOnCancellation {
+//            cancel()
+//        }
+//        enqueue(object : Callback<T> {
+//            override fun onResponse(call: Call<T>, response: Response<T>) {
+//                continuation.resume(response)
+//            }
+//
+//            override fun onFailure(call: Call<T>, t: Throwable) {
+//                continuation.resumeWithException(t)
+//            }
+//        })
+//    }
+//}
+//
+///**
+// * Force the calling coroutine to suspend before throwing [this].
+// *
+// * This is needed when a checked exception is synchronously caught in a [java.lang.reflect.Proxy]
+// * invocation to avoid being wrapped in [java.lang.reflect.UndeclaredThrowableException].
+// *
+// * The implementation is derived from:
+// * https://github.com/Kotlin/kotlinx.coroutines/pull/1667#issuecomment-556106349
+// */
+//internal suspend fun Exception.suspendAndThrow(): Nothing {
+//    suspendCoroutineUninterceptedOrReturn<Nothing> { continuation ->
+//        Dispatchers.Default.dispatch(continuation.context, Runnable {
+//            continuation.intercepted().resumeWithException(this@suspendAndThrow)
+//        })
+//        COROUTINE_SUSPENDED
+//    }
+//}
