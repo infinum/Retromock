82083ead31a67c281d50f8cd05e9331b90956ce4
BraneRi <branimir.akmadza@infinum.hr>
2020-04-30 09:27:17 +0200

    Add basic tests for suspend calls

--- retromock/src/test/kotlin/co/infinum/retromock/KotlinSuspendTest.kt
+++ retromock/src/test/kotlin/co/infinum/retromock/KotlinSuspendTest.kt
@@ -1,5 +1,7 @@
 package co.infinum.retromock
 
+import co.infinum.retromock.helpers.mock
+import co.infinum.retromock.helpers.whenever
 import co.infinum.retromock.meta.Mock
 import co.infinum.retromock.meta.MockResponse
 import org.assertj.core.api.Assertions
@@ -10,56 +12,86 @@ import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Path
 import kotlinx.coroutines.runBlocking
+import okhttp3.ResponseBody
+import org.junit.jupiter.api.TestInstance
+import org.mockito.Mockito
+import retrofit2.HttpException
+import java.nio.charset.StandardCharsets
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class KotlinSuspendTest {
 
   interface Service {
     @GET("/")
-    @MockResponse(body = "smith.json")
+    @MockResponse(body = "Body example.")
     @Mock
-    suspend fun body(): String
+    suspend fun body(): ResponseBody
 
     @GET("/")
-    @MockResponse(body = "smith.json")
+    @MockResponse(body = "Body example.")
     @Mock
-    suspend fun bodyNullable(): String?
+    suspend fun bodyNullable(): ResponseBody?
 
     @GET("/")
-    @MockResponse(body = "smith.json")
+    @MockResponse(body = "Body example.")
     @Mock
-    suspend fun response(): Response<String>
+    suspend fun response(): Response<ResponseBody>
 
     @GET("/{a}/{b}/{c}")
-    @MockResponse(body = "smith.json")
+    @MockResponse(body = "Body example.")
     @Mock
     suspend fun params(
             @Path("a") a: String,
             @Path("b") b: String,
             @Path("c") c: String
-    ): String
+    ): ResponseBody
   }
 
-  companion object {
+  lateinit var service: Service
 
-    lateinit var service: Service
-
-    @BeforeAll
-    fun setup() {
-      val retrofit = Retrofit.Builder()
-              .baseUrl("https://www.google.com")
-              .build()
-      val retromock: Retromock = Retromock.Builder()
-              .retrofit(retrofit)
-              .defaultBehavior(Behavior { 0 })
-              .build()
-      service = retromock.create(Service::class.java)
+  @BeforeAll
+  fun setup() {
+    val factory = mock<BodyFactory>()
+    whenever(factory.create(Mockito.anyString())).then {
+      (it.arguments[0] as String).byteInputStream(StandardCharsets.UTF_8)
     }
+
+    val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.google.com")
+            .build()
+    val retromock: Retromock = Retromock.Builder()
+            .retrofit(retrofit)
+            .defaultBodyFactory(factory)
+            .defaultBehavior(Behavior { 0 })
+            .build()
+    service = retromock.create(Service::class.java)
   }
 
   @Test
   fun body() {
     val body = runBlocking { service.body() }
-    Assertions.assertThat(body).isEqualTo("Hi")
+    Assertions.assertThat(body.string()).isEqualTo("Body example.")
+  }
+
+  @Test
+  fun body404() {
+    try {
+      runBlocking { service.body() }
+    } catch (e: HttpException) {
+      Assertions.assertThat(e.code()).isEqualTo(404)
+    }
+  }
+
+  @Test
+  fun bodyThrowsOnNull() {
+    try {
+      runBlocking { service.body() }
+    } catch (e: KotlinNullPointerException) {
+      // Coroutines wraps exceptions with a synthetic trace so fall back to cause message.
+      val message = e.message ?: (e.cause as KotlinNullPointerException).message
+      Assertions.assertThat(message).isEqualTo(
+              "Response from retrofit2.KotlinSuspendTest\$Service.body was null but response body type was declared as non-null")
+    }
   }
 }
 
