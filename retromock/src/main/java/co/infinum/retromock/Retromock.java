edc3b2177a8eb837ed3bd6512de901813627a904
BraneRi <branimir.akmadza@infinum.hr>
2020-05-11 14:55:21 +0200

    Update versions and divide CallWrapper into interface and factory.

--- retromock/src/main/java/co/infinum/retromock/Retromock.java
+++ retromock/src/main/java/co/infinum/retromock/Retromock.java
@@ -239,7 +239,7 @@ public final class Retromock {
             return method.invoke(delegate, args);
           }
 
-          AbstractCallWrapper callWrapper = AbstractCallWrapper.<T>create(method, args);
+          CallWrapper callWrapper = CallWrapperFactory.<T>create(method, args);
 
           final CallAdapter<?, T> callAdapter = (CallAdapter<?, T>) retrofit
             .callAdapter(callWrapper.getReturnType(), method.getAnnotations());
